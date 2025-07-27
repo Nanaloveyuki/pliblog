f2833cb71991cc2694e02b0ba4e5c60acb346245
Nanaloveyuki <miaomiao_159@outlook.com>
2025-07-27 14:52:15 +0800

    ✨ 添加了`config`读取,写入,重置功能

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+"""
+工具函数 / Utility functions
+"""
+# encoding = utf-8
+# python 3.13.5
+
+from .configs import get_config, set_config, reset_config
+
+__all__ = [
+    "get_config",
+    "set_config",
+    "reset_config"
+]
